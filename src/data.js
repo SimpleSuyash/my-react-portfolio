88c89e4b65d7b93e7ecee424f9cb8d9c3bf46512
SimpleSuyash <suyashmaharjan@gmail.com>
2024-04-10 12:45:00 +1000

    header navbar createdf

---
+++ src/data.js
@@ -0,0 +1,23 @@
+// navigation
+export const navigation = [
+    {
+      name: 'home',
+      href: 'home',
+    },
+    {
+      name: 'about',
+      href: 'about',
+    },
+    {
+      name: 'portfolio',
+      href: 'portfolio',
+    },
+    {
+      name: 'contact',
+      href: 'contact',
+    },
+    {
+        name: 'resume',
+        href: 'resume',
+    }
+  ];
