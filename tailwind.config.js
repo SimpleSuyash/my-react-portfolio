4c906860aeeefee81ace43de653fff011338fa16
SimpleSuyash <suyashmaharjan@gmail.com>
2024-04-10 20:34:19 +1000

    hero section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,21 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      primary: 'Playfair Display',
-      body: 'Work Sans',
+      primary: ['Playfair Display', 'serif'],
+      body: ['Barlow', 'sans-serif'],
+      logo: ['Great Vibes', 'cursive'],
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '3rem',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
     },
     extend: {
       colors: {
@@ -15,7 +28,8 @@ export default {
           DEFAULT: '#ac6b34',
           hover: '#925a2b',
         },
-        paragraph: '#878e99',
+        // paragraph: '#878e99',
+        paragraph: '#78716c',
       },
 
     },
